b09d85ec7521a21a3680e6a40f29d47155765223
Tom <tom.lauwaerts@hotmail.com>
2018-02-02 11:07:37 +0100

    improve build script

--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,17 @@
-sudo docker build -t skosmos/apache-php:v0.2 . # (in folder met dockerfile)
-sudo docker run -it -d --name="skosmos_v0.2d" skosmos/apache-php:v0.2d
-sudo docker exec -it skosmos_v0.2d bash
+#!/bin/bash
 
+image_name="skosmos/apache-php:v0.2"
+container_name="skosmos_v0.2"
+
+# remove old container
+echo "removing old container ..."
+sudo docker stop $container_name
+sudo docker rm $container_name
+sudo docker rmi $image_name
+
+# build container
+echo
+echo "building container ..."
+sudo docker build -t $image_name . # (in folder met dockerfile)
+sudo docker run -it -d --name=$container_name $image_name
+sudo docker exec -it $container_name bash
