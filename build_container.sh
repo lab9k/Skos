0026f5709acef9ff0f851008e6c23f38fc5d5908
Tom <tom.lauwaerts@hotmail.com>
2018-02-02 11:45:25 +0100

    add container to host network

--- build_container.sh
+++ build_container.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+# Shell scrip to automatically build the docker container for 
+# the https://github.com/lab9k/Skos repository. The container
+# contains a ubuntu 16.04 server with Skosmos and an example
+# vocabulary.
+#
+# WARNING: BE SURE TO RUN THIS SCRIPT IN THE FOLDER WITH THE
+# SUPPLIED DOCKERFILE!
+
 image_name="skosmos/apache-php:v0.2"
 container_name="skosmos_v0.2"
 
 # remove old container
-echo "removing old container ..."
+echo "removing..."
 sudo docker stop $container_name
 sudo docker rm $container_name
+
+# remove old image
 sudo docker rmi $image_name
+echo "DONE"
+
+echo
+
+echo "building..."
+# build image
+sudo docker build -t $image_name .
 
 # build container
-echo
-echo "building container ..."
-sudo docker build -t $image_name . # (in folder met dockerfile)
-sudo docker run -it -d --name=$container_name $image_name
+sudo docker run -itd --network=host --name=$container_name $image_name
+
+# attach to container with bash
 sudo docker exec -it $container_name bash
