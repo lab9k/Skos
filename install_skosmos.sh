77470e4b570edca00b2f2d1c9632fa5938e82a71
Jorg Wieme <jorg.wieme@ugent.be>
2018-02-05 12:42:43 +0100

    Update install_skosmos.sh

--- install_skosmos.sh
+++ install_skosmos.sh
@@ -1,8 +1,8 @@
-cd
+cd home
 wget https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-3.6.0.tar.gz
 cd /opt
-tar xzf ~/apache-jena-fuseki-3.6.0.tar.gz
-rm ~/apache-jena-fuseki-3.6.0.tar.gz
+tar xzf /home/apache-jena-fuseki-3.6.0.tar.gz
+rm /home/apache-jena-fuseki-3.6.0.tar.gz
 ln -s apache-jena-fuseki-3.6.0 fuseki
 cd /opt/fuseki
 adduser --system --home /opt/fuseki --no-create-home fuseki
@@ -37,15 +37,17 @@ cat skosmos.ttl >> /etc/fuseki/configuration/skosmos.ttl
 rm skosmos.ttl
 service fuseki start
 sleep 5
-cd 
+cd /home
 wget http://skos.um.es/unescothes/unescothes.ttl
 /opt/fuseki/bin/s-put http://127.0.0.1:3030/skosmos/data default unescothes.ttl
 mv /skosmos /var/www/html/
-cd
+rm unescothes.ttl
 > /etc/apache2/sites-enabled/000-default.conf
 wget https://raw.githubusercontent.com/lab9k/Skos/develop/Needed_files/Configure_apache
 cat Configure_apache >> /etc/apache2/sites-enabled/000-default.conf
 rm Configure_apache
+a2enmod rewrite
+service apache2 rewrite
 cd /var/www/html/skosmos
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install --no-dev
@@ -62,3 +64,7 @@ wget https://raw.githubusercontent.com/lab9k/Skos/develop/Needed_files/Vocabular
 cat Vocabularies >> vocabularies.ttl
 rm Vocabularies 
 service apache2 start
+sleep 2
+service fuseki start
+sleep 2
+# add vocbench script
