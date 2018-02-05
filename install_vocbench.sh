def953e5da1b4af297bf18991fe71c8495be0553
Maxime Van Driessche <van-driessche-maxime@hotmail.com>
2018-02-05 04:19:15 -0800

    added install_vocbench.sh

--- install_vocbench.sh
+++ install_vocbench.sh
@@ -1,7 +1,7 @@
 cd /home
 wget https://bitbucket.org/art-uniroma2/vocbench3/downloads/vocbench3_standalone-2.0.zip
 unzip vocbench3_standalone-2.0.zip
-cd /semanticturkey-2.0/
+cd semanticturkey-2.0/
 apt-get install -y dos2unix
 find . -type f -print0 | xargs -0 dos2unix
 chmod u+x bin/st_server_run
